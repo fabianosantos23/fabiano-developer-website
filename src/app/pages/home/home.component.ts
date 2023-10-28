03ea566918adc07bee0a5509f6dea4ec4bf87edb
Fabiano Santos <50252807+fabianosantos23@users.noreply.github.com>
2023-10-27 22:56:14 -0300

    feat: create projects section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  isLoading = true;
 
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
+
+  async ngAfterViewInit() {
+    console.time('Tempo de execução');
+    const mediaElements = this.elementRef.nativeElement.querySelectorAll('img, video');
+    let resourcesLoaded = 0;
+
+    await mediaElements.forEach((element: HTMLImageElement | HTMLVideoElement) => {
+      this.renderer.listen(element, 'load', () => {
+        resourcesLoaded++;
+
+        if (resourcesLoaded === mediaElements.length) {
+          // Todos os recursos foram carregados
+          this.isLoading = false;
+        }
+      });
+    });
+
+    console.timeEnd('Tempo de execução');
+  }
 }
